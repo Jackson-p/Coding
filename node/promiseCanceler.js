a8149126478eeb6cf92b0f42afdd96c063fa590b
201585052 <1216884492@qq.com>
2019-02-22 19:40:37 +0800

    prepwa

--- node/promiseCanceler.js
+++ node/promiseCanceler.js
@@ -1,3 +1,3 @@
 const fetch = require('node-fetch')
 
-fetch("https://api.github.com/repos/Jackson-p/Jackson-p.github.io/issues").then(res => res.json()).then(data => console.log(data))
+fetch(`https://api.github.com/search/issues?q=+state:open+repo:bingoogolapple/bingoogolapple.github.io+label:设计模式&sort=created&order=desc&page=1&per_page=6`).then(res => res.json()).then(data => console.log(data))
---
+++ node/src/print.js
@@ -0,0 +1,6 @@
+let printLiao = function(){
+    let keywords;
+    keywords = "Brelly";
+    console.log(keywords + "liaoliao");
+};
+export default printLiao;
